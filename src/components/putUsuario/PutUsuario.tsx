6808e0141741b6a8681590136714ac287e9c12e6
Matheus S. Oliveira <matheuskiller6@gmail.com>
2023-03-24 14:57:20 -0300

    ajustes finais

--- src/components/putUsuario/PutUsuario.tsx
+++ src/components/putUsuario/PutUsuario.tsx
@@ -59,7 +59,7 @@ function PutUsuario() {
     function updatedUsuario(e: ChangeEvent<HTMLInputElement>) {
 
         setUsuario({
-                ...usuario,
+            ...usuario,
             [e.target.name]: e.target.value,
         })
 
@@ -106,33 +106,42 @@ function PutUsuario() {
 
     return (
         <>
-            <Grid className='cointainer' container spacing={2}>
-                <Grid item xs={12}>
-                    <Box className="caixa">
-                        <form onSubmit={onSubmit}>
-                        {/* <TextField margin='normal' variant='outlined' value={usuario.id} fullWidth/> */}
-                        <Typography className='titulo' variant='h4' align='center'>Atualizar perfil</Typography>
-                        <TextField value={usuario.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedUsuario(e)} id="nome" label="Nome de usuario" variant="outlined" name="nome" margin="normal" fullWidth />
-                        <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedUsuario(e)} id="senha" label="senha" variant="outlined" name="senha" margin="normal" fullWidth />
-                        <TextField label='Comfirmar seha' id='nome' name='nome' variant='outlined' fullWidth  />
-                        <TextField value={usuario.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedUsuario(e)} id="foto" label="foto" variant="outlined" name="foto" margin="normal" fullWidth />
-                        <Button type='submit' variant='contained' color='primary'>Atualizar perfil</Button>
-                        </form>
-                    </Box>
+
+            <Grid container>
+                <Grid container item
+                    xs={12}
+                    style={{
+                        backgroundImage: `url(https://images.303magazine.com/uploads/2019/08/GettyImages-510083766.jpg)`,
+
+                        // https://img.freepik.com/fotos-gratis/bela-vista-de-uma-pequena-cidade-nas-montanhas-durante-o-por-do-sol-no-brasil_181624-39388.jpg?w=1380&t=st=1678927973~exp=1678928573~hmac=9d10b84295ac61de991523135d7fdb7e7004d250801a39d01d73fe8065dc2a09
+
+                        backgroundRepeat: "no-repeat",
+                        width: "100vh",
+                        minHeight: "100vh",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                >
+                    <Grid className='cointainer' container justifyContent="center" alignItems="center" spacing={2}>
+                        <Grid item justifyContent="center" alignItems='center' xs={12}>
+                            <Box paddingX={20} className="fundo-transparent">
+                                <form onSubmit={onSubmit}>
+                                    {/* <TextField margin='normal' variant='outlined' value={usuario.id} fullWidth/> */}
+                                    <Typography className='titulo-put-usuario' variant='h4' align='center'>Atualizar perfil</Typography>
+                                    <TextField value={usuario.nome} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedUsuario(e)} id="nome" label="Nome de usuario" variant="outlined" name="nome" margin="normal" fullWidth />
+                                    <TextField onChange={(e: ChangeEvent<HTMLInputElement>) => updatedUsuario(e)} id="senha" label="senha" type='password' variant="outlined" name="senha" margin="normal" fullWidth />
+                                    <TextField label='Confirmar senha' id='nome' type='password' name='nome' variant='outlined' fullWidth />
+                                    <TextField value={usuario.foto} onChange={(e: ChangeEvent<HTMLInputElement>) => updatedUsuario(e)} id="foto" label="foto" variant="outlined" name="foto" margin="normal" fullWidth />
+                                    <Button type='submit' variant='contained' color='primary'>Atualizar perfil</Button>
+                                </form>
+                            </Box>
+                        </Grid>
+                  
+                  </Grid> 
+                 </Grid>
                 </Grid>
-
-                <Grid item xs={12}>
-                    <Box className='posts'>
-
-                    </Box>
-                </Grid>
-
-                <Grid item xs={8}>
-
-                </Grid>
-            </Grid>
-        </>
-    )
+            </>
+            )
 
 }
-export default PutUsuario;
+            export default PutUsuario;
