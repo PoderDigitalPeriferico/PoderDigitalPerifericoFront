be10a83d8022b66c8328fc36547e74e4ea15f0cc
vanessa.isabella.oliveira@gmail.com <caiqueiamatsuka@gmail.com>
2023-03-24 19:28:22 -0300

    Ajustando sobre nós, card reposicionado e add linkedin/github

--- src/paginas/SobreOProjeto/SobreOProjeto.tsx
+++ src/paginas/SobreOProjeto/SobreOProjeto.tsx
@@ -6,9 +6,10 @@ import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../store/tokens/tokensReducer';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 function SobreOProjeto() {
-    
     const token = useSelector<TokenState, TokenState['tokens']>(
         (state) => state.tokens
     );
@@ -30,13 +31,13 @@ function SobreOProjeto() {
 
         }
     }, [token])
-    
+
     return (
         <>
             <Box className="parallax">
                 <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela1.png?updatedAt=1679061328198" alt="camada 1 da figura da favela" id="favela1" />
                 <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela6.png?updatedAt=1679061386886" alt="camada 6 da figura da favela" id='favela6' />
-                <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela2.png?updatedAt=1679061412062"    alt="camada 2 da figura da favela" id='favela2'  />
+                <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela2.png?updatedAt=1679061412062" alt="camada 2 da figura da favela" id='favela2' />
                 <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela3.png?updatedAt=1679061411959" alt="camada 3 da figura da favela" id='favela3' />
                 <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela4.png?updatedAt=1679061411776" alt="camada 4 da figura da favela" id='favela4' />
                 <img src="https://ik.imagekit.io/vinikills/IMAGENS_SOBRE_O_PROJETO/favela5.png?updatedAt=1679061412613" alt="camada 5 da figura da favela" id='favela5' />
@@ -58,162 +59,125 @@ function SobreOProjeto() {
                 <h2>Poder Digital Periférico</h2>
                 <div>
                     <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit 
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique error a incidunt. Quisquam voluptatibus eius sit
                         fugit saepe vero. Molestias omnis eligendi officiis totam aperiam, commodi sunt odio accusantium doloremque!
                     </p>
                 </div>
+                <h2>Desenvolvedores</h2>
             </Grid>
 
-            <div className="integrantes">
-                <div className="card-container">
-                    <div className="card-info">
+            <div className="desenvolvedores">
+                <div className="integrantes">
+                    <div className="card-container">
                         <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/adao.jpg?updatedAt=1679610027008" alt="user" />
-                        <h3>Adão</h3>
+                        <h3>Adão Torres</h3>
                         <h6>São Paulo</h6>
                         <p>Desenvolvedor front-end e back-end</p>
                         <div className="buttons">
-                            <button className="primary">
-                                Message
-                            </button>
-                            <button className="primary ghost">
-                                Following
-                            </button>
+                            <a href=" LINKEDIN ADAO" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/AdaoTorres" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
                         </div>
                     </div>
-                    <div className="card-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium. 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.</p>
-                    </div>
-                </div>
 
-                <div className="card-container">
-                    <div className="card-info">
+                    <div className="card-container">
                         <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/caiqueBack.jpg?updatedAt=1679605343559" alt="user" />
-                        <h3>Caique</h3>
+                        <h3>Caique Motta</h3>
                         <h6>São Paulo</h6>
                         <p>Desenvolvedor front-end e back-end</p>
                         <div className="buttons">
-                            <button className="primary">
-                                Message
-                            </button>
-                            <button className="primary ghost">
-                                Following
-                            </button>
+
+                            <a href="https://www.linkedin.com/in/caique-motta-1682ab14b/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/Iamatsuka" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
                         </div>
                     </div>
-                <div className="card-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium. 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.</p>
-                    </div>
-                </div>
 
-
-                
-                <div className="card-container">
-                    <div className="card-info">
-                        <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/fernando_1_.jpg?updatedAt=1679610230621" alt="user" />
-                        <h3>Fernando</h3>
+                    <div className="card-container">
+                        <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/120676803.jfif?updatedAt=1679692032579" alt="user" />
+                        <h3>Fernando Azevedo</h3>
                         <h6>São Paulo</h6>
                         <p>Desenvolvedor front-end e back-end</p>
                         <div className="buttons">
-                            <button className="primary">
-                                Message
-                            </button>
-                            <button className="primary ghost">
-                                Following
-                            </button>
+                            <a href="https://www.linkedin.com/in/fernandodazevedo/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/fernandodelgadoazevedo" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
                         </div>
-                    </div>
-                    <div className="card-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium. 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.</p>
                     </div>
                 </div>
 
-                <div className="card-container">
-                    <div className="card-info">
+                <div className="integrantes">
+                    <div className="card-container">
                         <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/matheus.jpg?updatedAt=1679610027722" alt="user" />
-                        <h3>Matheus</h3>
+                        <h3>Matheus Oliveira</h3>
                         <h6>São Paulo</h6>
                         <p>Desenvolvedor front-end e back-end</p>
                         <div className="buttons">
-                            <button className="primary">
-                                Message
-                            </button>
-                            <button className="primary ghost">
-                                Following
-                            </button>
+                            <a href="https://www.linkedin.com/in/dev-matheus-oliveira/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/1DevMatheus" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
                         </div>
                     </div>
-                    <div className="card-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium. 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.</p>
-                    </div>
-                </div>
 
-                
-                <div className="card-container">
-                    <div className="card-info">
+
+                    <div className="card-container">
                         <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/rafael.jpg?updatedAt=1679610024408" alt="user" />
-                        <h3>Rafael</h3>
+                        <h3>Rafael Souza</h3>
                         <h6>São Paulo</h6>
                         <p>Desenvolvedor front-end e back-end</p>
                         <div className="buttons">
-                            <button className="primary">
-                                Message
-                            </button>
-                            <button className="primary ghost">
-                                Following
-                            </button>
+                            <a href="https://www.linkedin.com/in/rafael-souza-686862118/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/VasoRuim011" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
                         </div>
                     </div>
-                    <div className="card-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium. 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.</p>
-                    </div>
-                </div>
 
-                <div className="card-container">
-                    <div className="card-info">
+                    <div className="card-container">
                         <img className="round" src="https://ik.imagekit.io/8j6bttpzpq/vinicius.jpg?updatedAt=1679610025929" alt="user" />
-                        <h3>Vinícius</h3>
+                        <h3>Vinícius Fernando</h3>
                         <h6>São Paulo</h6>
                         <p>Desenvolvedor front-end e back-end</p>
                         <div className="buttons">
-                            <button className="primary">
-                                Message
-                            </button>
-                            <button className="primary ghost">
-                                Following
-                            </button>
+                            <a href="https://www.linkedin.com/in/https:/www.linkedin.com/in/viniferan/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
+                            <a href="https://github.com/Vinikills" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon style={{ fontSize: 55, color: "white" }} />
+                            </a>
                         </div>
-                    </div>
-                    <div className="card-text">
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium. 
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A odit velit atque libero voluptatum, est, dolor consequatur amet pariatur corporis ea suscipit aut dicta quam perspiciatis ipsum! Quae, molestias praesentium.</p>
                     </div>
                 </div>
             </div>
+
 
             <Helmet>
                 <script src="Script/script.js" type="text/javascript" />
